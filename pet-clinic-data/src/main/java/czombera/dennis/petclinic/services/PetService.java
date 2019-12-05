7a3d80c26e7b41e0293b9776b1c96cb900611c20
Dennis Czombera <milchschaumschluerfer@gmail.com>
2019-12-05 21:01:19 +0100

    Implement interfaces for services

---
+++ pet-clinic-data/src/main/java/czombera/dennis/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package czombera.dennis.petclinic.services;
+
+import czombera.dennis.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
