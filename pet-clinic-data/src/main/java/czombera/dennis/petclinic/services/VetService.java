6e35ba57f26655356c273f8a9502eec7e212cb64
Dennis Czombera <milchschaumschluerfer@gmail.com>
2019-12-06 15:39:48 +0100

    Refactor to common crud service interface

--- pet-clinic-data/src/main/java/czombera/dennis/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/czombera/dennis/petclinic/services/VetService.java
@@ -2,12 +2,5 @@ package czombera.dennis.petclinic.services;
 
 import czombera.dennis.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
