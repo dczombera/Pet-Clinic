f7f41671fc87d5aeef9f166efa3c98304e9eae23
Dennis Czombera <milchschaumschluerfer@gmail.com>
2019-12-13 11:34:52 +0100

    Add visit data to data loader

--- pet-clinic-web/src/main/java/czombera/dennis/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/czombera/dennis/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package czombera.dennis.petclinic.bootstrap;
 
 import czombera.dennis.petclinic.model.*;
-import czombera.dennis.petclinic.services.OwnerService;
-import czombera.dennis.petclinic.services.PetTypeService;
-import czombera.dennis.petclinic.services.SpecialtyService;
-import czombera.dennis.petclinic.services.VetService;
+import czombera.dennis.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +13,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -85,6 +84,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Snezzing");
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
