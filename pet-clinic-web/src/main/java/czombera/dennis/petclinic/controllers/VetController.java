1fea39c71fce6fb173a8755522560165309cb0b6
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-01-13 16:45:12 +0100

    Add api endpoint for vets; extend data binder of visit controller

--- pet-clinic-web/src/main/java/czombera/dennis/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/czombera/dennis/petclinic/controllers/VetController.java
@@ -1,12 +1,16 @@
 package czombera.dennis.petclinic.controllers;
 
+import czombera.dennis.petclinic.model.Vet;
 import czombera.dennis.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/vets")
+import java.util.Set;
+
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -16,9 +20,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
